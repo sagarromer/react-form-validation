724e29f330dbd2fa06d1546e000ef5a872846d3a
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-06 01:40:46 -0700

    initial commit for the form validation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Main from './Main'
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div>
+            form validation app
+        </div>
+    )
+}
+
+export default Main
