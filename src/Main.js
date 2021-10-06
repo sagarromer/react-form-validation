5f6c701b374b799fd12d440d9d7eaff34d6bb72d
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-06 02:40:55 -0700

    basic form created with bootstrap styles

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,16 @@ import React from 'react'
 function Main() {
     return (
         <div>
-            form validation app
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <h1>form validation</h1>
+                    <input type="text" placeholder='username' className="form-control"/>
+                    <input type="text" placeholder='email' className="form-control"/>
+                    <input type="text" placeholder='password' className="form-control"/>
+                    <input type="text" placeholder='confirm password' className="form-control"/>
+                    <button className="btn btn-success">submit</button>                    
+                </div>
+            </div>
         </div>
     )
 }
